98917c3d19f50c5a2fb659407c2653a3abf76f94
Bill Spencer <badgley@gmail.com>
2016-04-30 23:36:23 -0400

    minor fix

---
+++ src/shapes/Circle.java
@@ -0,0 +1,34 @@
+package shapes;
+import java.text.DecimalFormat;
+
+public class Circle {
+
+	private double radius;
+	private DecimalFormat df2 = new DecimalFormat(".##");
+
+	public Circle(double r) {
+		radius = r;
+	}
+
+	public double getArea() {
+		return Math.PI * radius * radius;
+	}
+
+	public double getCircumference() {
+		return (2 * Math.PI * radius);
+
+	}
+
+	private String formatNumber(double x) {
+		return df2.format(x);
+
+	}
+	
+	public String getFormattedCircumference() {
+		return formatNumber(getCircumference());
+	}
+
+	public String getFormattedArea() {
+		return formatNumber(getArea());
+	}
+}
